8947b4f94d9c17e8b1f75b04a1b4f4ddb25a780f
JAYESH-U <uramanattijayesh@gmail.com>
2022-11-04 18:45:23 +0530

    literals

---
+++ Literals.java
@@ -0,0 +1,16 @@
+public class Literals {
+    public static void main(String[] args) {
+        byte age = 34;  //set default
+        int age2 = 56;  //,,
+        short age3 = 87;    //,,
+        long ageDino = 5666666666666L;  //L(by default it is set to int type)
+        char ch = 'A';  //set default and (' ')--> char literal
+        float f1 = 5.6f;    //set double by default (to set it to float add 'f' or 'F')
+        double d1 = 4.66;   //set double by default (d or D)
+        boolean a = true;      //true or false -->boolean literal
+        System.out.println(age);
+        System.out.println(age2);
+        String str = "Harry";   //" " --> string literal
+        System.out.println(str);
+    }
+}
