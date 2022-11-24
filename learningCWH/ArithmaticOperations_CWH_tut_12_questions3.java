d22bade3a0ef1d4ad91453c780d106dfdcc37c90
JAYESH-U <uramanattijayesh@gmail.com>
2022-11-24 20:27:12 +0530

    delete

--- learningCWH/ArithmaticOperations_CWH_tut_12_questions3.java
+++
@@ -1,8 +0,0 @@
-public class ArithmaticOperations_CWH_tut_12_questions3
-{
-    public static void main(String[] args) {
-        float a = 7/4.0f * 9/2.0f;  //.0f to make the answer more accurate and precise.
-        System.out.println(a);
-        System.out.println(7*49/7.0f+35/7.0f);
-    }
-}
