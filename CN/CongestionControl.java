02718840ebaa66880413ddd0cb3402acd3919a3b
Jayesh <uramanattijayesh@gmail.com>
2023-12-19 19:53:52 +0530

    TW ab

--- CN/CongestionControl.java
+++ CN/CongestionControl.java
@@ -9,57 +9,57 @@ public class CongestionControl {
         n = scanner.nextInt();
 
         int size[] = new int[n];
-        System.out.print("\nEnter sizes:");
+        System.out.print("\nEnter sizes : ");
 
         for (j = 0; j < n; j++) {
             size[j] = scanner.nextInt();
         }
 
-        System.out.print("\nEnter the size of the bucket:");
+        System.out.print("\nEnter the size of the bucket : ");
         s = scanner.nextInt();
 
-        System.out.println("\nEnter output rate:");
+        System.out.print("\nEnter output rate : ");
         rate = scanner.nextInt();
 
         for (i = 0; i < n; i++) {
-            System.out.println("\n" + "TIME " + c);
+            System.out.println("TIME : " + c);
 
             if (size[i] > s) {
-                System.out.println("\n" + "dropped" + c + "\n");
+                System.out.println("dropped : " + c + "\n");
             } else if ((size[i] + rem) > s) {
-                System.out.println("\n" + "dropped" + c + "\n");
+                System.out.println("dropped : " + c + "\n");
             } else {
                 rem = rem + size[i];
             }
 
             if (rem < rate)
-                System.out.println("\n" + "bytes txmitted " + rem + "\n");
+                System.out.println("bytes txmitted : " + rem + "\n");
             else
-                System.out.println("\n" + "bytes txmitted " + rate + "\n");
+                System.out.println("bytes txmitted : " + rate + "\n");
 
             rem -= rate;
 
             if (rem <= 0)
                 rem = 0;
             else
-                System.out.println("\n" + "bytes txmitted" + rem + "\n");
+                System.out.println("bytes txmitted : " + rem + "\n");
 
             c++;
         }
 
         while (rem != 0) {
-            System.out.println("\n" + "TIME" + c + "\n");
+            System.out.println("TIME : " + c + "\n");
             c++;
 
             if (rem < rate) {
-                System.out.println("\n" + "bytes txmitted" + rem + "\n");
+                System.out.println("bytes txmitted : " + rem + "\n");
                 rem = 0;
             }
 
             if (rem >= rate) {
-                System.out.println("\n" + "bytes txmitted" + rate + "\n");
+                System.out.println("bytes txmitted : " + rate + "\n");
                 rem -= rate;
-                System.out.println("\n" + "bytes txmitted" + rem + "\n");
+                System.out.println("bytes txmitted : " + rem + "\n");
             }
         }
 
