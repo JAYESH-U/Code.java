4b04a70e3fccb97de9cf0788b1b6cc37a884846a
Jayesh <uramanattijayesh@gmail.com>
2024-02-13 17:00:30 +0530

    work in progress

---
+++ advJava/labPractice/TW7and8/src/BankTransaction.java
@@ -0,0 +1,15 @@
+package advJava.labPractice.TW7and8.src;
+
+public class BankTransaction {
+
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver"; // MySQL JDBC driver
+    static final String DB_URL = "jdbc:mysql://localhost:3306/"; // 3306 is the default port for MySQL
+    static final String DB_NAME = "company";
+    static final String USER = "root"; // Username and password
+    static final String PASS = "jayesh2003"; // You should change these to your own
+    
+
+    public static void main(String[] args) {
+        
+    }
+}
